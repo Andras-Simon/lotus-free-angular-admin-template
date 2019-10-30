6c9065b618d933beea31819e57dd77fac9fe1acb
Andras <andras@cabsolutely.co>
2019-10-30 18:03:02 +0100

    Add collapsible menu items

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,11 @@ const sidenavEl = document.querySelector('.menu');
 const toggle = () => sidenavEl.classList.toggle('active');
 document.querySelector('.header__menu-open').addEventListener('click', toggle);
 document.querySelector('.menu__menu-close').addEventListener('click', toggle);
+
+
+document.querySelectorAll('.menu__item--collapsible').forEach(el => {
+    el.addEventListener('click', () => {
+        el.classList.toggle('active');
+        el.childNodes.forEach(child => child.classList && child.classList.toggle('active'))
+    });
+});
