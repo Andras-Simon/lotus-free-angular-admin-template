348ceb120660ad9a022a6d67ba4aaee526a828be
Andras <andras@cabsolutely.co>
2019-11-04 18:15:45 +0100

    Refactor main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,9 @@ const toggle = () => sidenavEl.classList.toggle('active');
 document.querySelector('.header__menu-open').addEventListener('click', toggle);
 document.querySelector('.menu__menu-close').addEventListener('click', toggle);
 
-
 document.querySelectorAll('.menu__item--collapsible').forEach(el => {
-    el.addEventListener('click', () => {
-        el.classList.toggle('active');
-        el.childNodes.forEach(child => child.classList && child.classList.toggle('active'))
-    });
+  el.addEventListener('click', () => {
+    el.classList.toggle('active');
+    el.childNodes.forEach(child => child.classList && child.classList.toggle('active'))
+  });
 });
